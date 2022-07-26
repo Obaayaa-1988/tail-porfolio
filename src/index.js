89a321021a37fa5546c451fd47f8ca8937132e20
Abigail <ashbella333@gmail.com>
2022-07-26 11:28:43 +0000

    completed blog recipe

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const burger = document.querySelector("#burger")
+const menu = document.querySelector("#menu")
+
+// burger.addEventListener("click", () => {
+//     if (menu.classList.contains("hidden")) {
+//         menu.style.display = "block"
+
+//     } else {
+//         menu.style.display = "hidden"
+//     }
+
+// })
+
+
+
+burger.addEventListener("click", () => {
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden")
+
+    } else {
+        menu.classList.add("hidden")
+
+    }
+
+})
